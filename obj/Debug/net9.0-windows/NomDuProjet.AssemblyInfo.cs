990ee4d80f3130f19f4c8aafa4381a9b11790dab
unknown <se55233@gmail.com>
2024-12-15 21:45:58 +0100

    testecommit

--- obj/Debug/net9.0-windows/NomDuProjet.AssemblyInfo.cs
+++ obj/Debug/net9.0-windows/NomDuProjet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NomDuProjet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4dd50182c7001e9c8dc74293929f5936dcffa0b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("NomDuProjet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NomDuProjet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
